c109e7648025b86b7586ad4eff821e6c2eada05f
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-25 10:39:33 -0300

    Initial

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div className="app__wrapper">
+            
+        </div>
+    );
+}
+
+export default App;
